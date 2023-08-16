b222aa3298cd5069141d4d359fa3a9fd4d4ea496
samsperd <iloejeobinna@gmail.com>
2023-08-16 03:06:04 +0100

    committed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-import { decompress } from 'brotli-compress'
+// import { decompress } from 'brotli-compress'
 
 
 const app = express();
