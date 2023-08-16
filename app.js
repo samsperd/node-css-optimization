d552016f2a5e9378c1508f6ca06a65ffac133cf6
samsperd <iloejeobinna@gmail.com>
2023-08-16 04:00:58 +0100

    committedt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// import { Buffer } from 'buffer'
 const express = require('express');
 const bodyParser = require('body-parser');
 const { decompress } = require('brotli-compress')
+const { Buffer } = require('buffer')
 
 
 const app = express();
@@ -15,13 +15,13 @@ app.post('/modify-html', (req, res) => {
     const { compressedHTML } = req.body;
     // console.log(req.body);
     // console.log(compressedHTML);
-    // const decompressedBuffer = decompress(Buffer.from(compressedHTML, 'base64'));
+    const decompressedBuffer = decompress(Buffer.from(compressedHTML, 'base64'));
 
-    // // // Process the decompressed HTML (you can modify this part)
-    // const processedHtml = decompressedBuffer.toString('utf-8');
+    // // Process the decompressed HTML (you can modify this part)
+    const processedHtml = decompressedBuffer.toString('utf-8');
 
-    // // console.log("processed HTML", processedHtml);
-    // console.log(processedHtml);
+    // console.log("processed HTML", processedHtml);
+    console.log(processedHtml);
 
 
     // Send back the modified HTML content as a response
