abfe9accdac02648cf61208556fc034ac8efbd80
samsperd <iloejeobinna@gmail.com>
2023-08-16 04:09:09 +0100

    committedt

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.post('/modify-html', async (req, res) => {
   try {
     const { compressedHTML } = req.body;
     // console.log(req.body);
-    // console.log(compressedHTML);
-    const decompressedBuffer = await decompress(Buffer.from(compressedHTML, 'base64'));
+    console.log(compressedHTML);
+    // const decompressedBuffer = await decompress(Buffer.from(compressedHTML, 'base64'));
 
-    // // Process the decompressed HTML (you can modify this part)
-    const processedHtml = decompressedBuffer.toString('utf-8');
+    // // // Process the decompressed HTML (you can modify this part)
+    // const processedHtml = decompressedBuffer.toString('utf-8');
 
-    // console.log("processed HTML", processedHtml);
-    console.log(processedHtml);
+    // // console.log("processed HTML", processedHtml);
+    // console.log(processedHtml);
 
 
     // Send back the modified HTML content as a response
